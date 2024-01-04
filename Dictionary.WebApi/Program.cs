bfb02433660ed485e15f2e6b4421c429ede9ec3d
Marta Zareckaja <m.zareckaja@adform.com>
2024-01-04 13:20:32 +0200

    Implemented background running method for cleanup functionality.

--- Dictionary.WebApi/Program.cs
+++ Dictionary.WebApi/Program.cs
@@ -1,9 +1,14 @@
+using Dictionary.WebApi.Interfaces;
+using Dictionary.WebApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IItemService, ItemService>();
+builder.Services.AddHostedService<CleanupHostedService>();
 
 var app = builder.Build();
 
--- Dictionary.WebApi/Services/ItemService.cs
+++ Dictionary.WebApi/Services/ItemService.cs
@@ -1,7 +1,13 @@
-﻿namespace Dictionary.WebApi.Services
-{
-    public class ItemService
-    {
+﻿using Dictionary.WebApi.Interfaces;
 
+namespace Dictionary.WebApi.Services
+{
+    public class ItemService : IItemService
+    {
+        public async Task CleanupAsync()
+        {
+            // Get db records, where expiration date is past NOW.
+            // Remove them from database.          
+        }
     }
 }
