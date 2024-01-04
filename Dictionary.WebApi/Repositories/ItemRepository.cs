20166c18f7ffa0fa4de6dd898905dc73f710ba68
Marta Zareckaja <m.zareckaja@adform.com>
2024-01-04 14:47:04 +0200

    Implemented Item service cleanup function.

--- Dictionary.WebApi/Repositories/ItemRepository.cs
+++ Dictionary.WebApi/Repositories/ItemRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace Dictionary.WebApi.Repositories
+﻿using Dapper;
+using Dictionary.WebApi.Interfaces;
+using Dictionary.WebApi.Models.Entities;
+using System.Data;
+
+namespace Dictionary.WebApi.Repositories
 {
-    public class ItemRepository
+    public class ItemRepository : IItemRepository
     {
+        private readonly IDbConnection _dbConnection;
+
+        public ItemRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<List<Item>> GetItemsAsync()
+        {
+            string query = @"SELECT 
+                            id,
+                            key,
+                            value,
+                            expires_at,
+                            expiration_period
+                            FROM items";
+
+            var items = await _dbConnection.QueryAsync<Item>(query);
+            return items.ToList();
+        }
+
+        public async Task DeleteItemsAsync(int id)
+        {
+            string query = @"DELETE FROM items WHERE id = @id;";
+            var queryArguments = new { id };
+
+            await _dbConnection.ExecuteAsync(query, queryArguments);
+        }
     }
 }
--- Dictionary.WebApi/Services/ItemService.cs
+++ Dictionary.WebApi/Services/ItemService.cs
@@ -4,10 +4,22 @@ namespace Dictionary.WebApi.Services
 {
     public class ItemService : IItemService
     {
+        private readonly IItemRepository _repository;
+
+        public ItemService(IItemRepository repository)
+        {
+            _repository = repository;
+        }
+
         public async Task CleanupAsync()
         {
-            // Get db records, where expiration date is past NOW.
-            // Remove them from database.          
+            var items = await _repository.GetItemsAsync();
+
+            foreach (var item in items)
+            {
+                if (item.ExpiresAt < DateTime.Now)
+                    await _repository.DeleteItemsAsync(item.Id);
+            }
         }
     }
 }
