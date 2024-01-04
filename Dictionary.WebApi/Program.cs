8f939a882d454f1da55a22047a8a2d45ebed46c7
Ugnius Adomavičius <u.adomavicius@adform.com>
2024-01-04 13:35:48 +0200

    db setup

--- Dictionary.WebApi/Program.cs
+++ Dictionary.WebApi/Program.cs
@@ -1,9 +1,26 @@
+using DbUp;
+using Npgsql;
+using System.Data;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration["MySecrets:PostgreConnection"] ?? throw new ArgumentNullException("Connection string was not found."); ;
+builder.Services.AddTransient<IDbConnection>(sp => new NpgsqlConnection(connectionString));
+// DbUp
+EnsureDatabase.For.PostgresqlDatabase(connectionString);
+var upgrader = DeployChanges.To
+        .PostgresqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToNowhere()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
 
 var app = builder.Build();
 
