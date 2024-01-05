07c7f72b5bb5cfd4ce4c56f4e5888382eb3cdf62
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2024-01-05 14:51:32 +0200

    added helper class for updateing and validating expiration time

---
+++ Dictionary.WebApi/Helpers/UpdateExpiresAtAndExpirationPeriod.cs
@@ -0,0 +1,25 @@
+﻿using Dictionary.WebApi.Models.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace Dictionary.WebApi.Helpers
+{
+    public class UpdateExpiresAtAndExpirationPeriod
+    {
+        public DateTime UpdateExpirationTime(int expirationPeriod)
+        {
+            return DateTime.UtcNow.AddSeconds(expirationPeriod);
+        }
+        public int GetValidExpirationPeriod(IConfiguration configuration, int? expirationPeriod)
+        {
+            int defaultExpirationPeriod = configuration.GetValue<int>("DefaultValues:DefaultExpirationValue");
+            if (expirationPeriod.HasValue && expirationPeriod <= defaultExpirationPeriod)
+            {
+                return expirationPeriod.Value;
+            }
+            else
+            {
+                return defaultExpirationPeriod;
+            }
+        }
+    }
+}
